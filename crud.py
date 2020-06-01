c18938ee4a7c6080df739fddf015c9158f0ff653
Julie Coleman <juliecoleman820@gmail.com>
2020-06-01 12:46:24 -0700

    Added users and movies routes and templates

--- crud.py
+++ crud.py
@@ -26,7 +26,31 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def all_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+
+def get_movie_by_id(movie_id):
+    """Return an individual movie."""
+
+    return Movie.query.get(movie_id)
+
+def all_users():
+    """Return all users."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return an individual movie."""
+
+    return User.query.get(user_id)
+
+
 def create_rating(user, movie, score):
+    """Create and return a rating by a user for a movie."""
 
     rating = Rating(user=user, movie=movie, score=score)
 
--- server.py
+++ server.py
@@ -12,11 +12,43 @@ app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 
-@app.route('/homepage')
-def homepage()
+@app.route('/')
+def homepage():
     """View homepage."""
 
     return render_template('homepage.html')
+
+@app.route('/movies')
+def all_movies():
+    """View all movies."""
+
+    movies = crud.all_movies()
+
+    return render_template('all_movies.html', movies=movies)
+
+@app.route('/movies/<movie_id>')
+def show_movie(movie_id):
+    """Show details on a particular movie."""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template('movie_details.html', movie=movie)
+
+@app.route('/users')
+def all_users():
+    """View all movies."""
+
+    users = crud.all_users()
+
+    return render_template('all_users.html', users=users)
+
+@app.route('/users/<user_id>')
+def show_user(user_id):
+    """Show details on a particular movie."""
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template('user_details.html', user=user)
 
 
 if __name__ == '__main__':
